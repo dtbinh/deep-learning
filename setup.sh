5b77283fa375d6d71c77d99768070aba2a4caaf5
Andrea Jemmett <andreajemmett@gmail.com>
2016-03-10 10:59:03 +0100

    some cleanup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-virtualenv ./
-source bin/activate
 pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.7.1-cp27-none-linux_x86_64.whl
 pip install ipython
+pip install jupyter
